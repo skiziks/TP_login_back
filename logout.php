18b390d080e96002d0f2dc331915309ccc5d3370
skiziks <82734149+skiziks@users.noreply.github.com>
2023-12-09 23:53:44 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_destroy();
 echo "Vous êtes déconnecté";
 echo "<br> <a href='login.php'>Se connecter</a>";
 
-
+// Exercice 3 
  if (isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
      session_destroy();
      header('Location: ./login.php');
