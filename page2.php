1a8d9fc082a952c4126f1f32de15b80a70f1b600
skiziks <82734149+skiziks@users.noreply.github.com>
2023-12-09 12:28:31 +0100

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,7 +1,8 @@
 <?php session_start();
 if (isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['email']) && isset($_SESSION['heure'])) {
     $date = date('d/m/Y H:i:s', $_SESSION['heure']);
-    echo "Nom : " . $_SESSION['nom'] . "<br> Prénom : " . $_SESSION['prenom'] . "<br> Email : " . $_SESSION['email'] . "<br> Heure : " . $date;
+    echo "Nom : " . $_SESSION['nom'] . "<br> Prénom : " . $_SESSION['prenom'] 
+        . "<br> Email : " . $_SESSION['email'] . "<br> Heure : " . $date;
 }
 
 echo "<br><a href='./logout.php'>Déconnexion</a>";
