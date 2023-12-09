a54b73c69364262f50b839d7eed955dfd669c43e
skiziks <82734149+skiziks@users.noreply.github.com>
2023-12-09 13:24:24 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,11 +4,11 @@ echo "Vous êtes déconnecté";
 echo "<br> <a href='login.php'>Se connecter</a>";
 
 
-// if (isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
-//     session_destroy();
-//     header('Location: ./login.php');
-//     exit();
-// } else {
-//     header('Location: ./login.php');
-//     exit();
-// }
+ if (isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
+     session_destroy();
+     header('Location: ./login.php');
+     exit();
+ } else {
+     header('Location: ./login.php');
+     exit();
+ }
